134408df488a430b40c3b9fac8ae334a38e22414
Shaik Zhafir <shaikzhafir@Shaiks-MacBook-Air.local>
2023-03-19 16:53:56 +0800

    - changed server listening on huproxy.go from 127.0.0.1 to 0.0.0.0 so rpi can reach server

--- cmd/huproxy/huproxy.go
+++ cmd/huproxy/huproxy.go
@@ -16,7 +16,6 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"io"
 	"net"
 	"net/http"
@@ -30,7 +29,7 @@ import (
 )
 
 var (
-	listen           = flag.String("listen", "127.0.0.1:8086", "Address to listen to.")
+	listen           = flag.String("listen", "0.0.0.0:8086", "Address to listen to.")
 	dialTimeout      = flag.Duration("dial_timeout", 10*time.Second, "Dial timeout.")
 	handshakeTimeout = flag.Duration("handshake_timeout", 10*time.Second, "Handshake timeout.")
 	writeTimeout     = flag.Duration("write_timeout", 10*time.Second, "Write timeout.")
@@ -40,9 +39,8 @@ var (
 )
 
 func handleProxy(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithCancel(r.Context())
-	defer cancel()
-	// Upgrade the HTTP connection to a websocket connection.
+	log.Println("handleProxy called")
+	// Upgrade the HTTP connection from rpi client to a websocket connection.
 	wsConn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Warningf("Failed to upgrade to websockets: %v", err)
@@ -50,78 +48,77 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 	}
 	defer wsConn.Close()
 
-	// receives the request from rpi websocket client, start a TCP server so other clients on the same network can connect to it
-	// and then forwards the request to the TCP server and forwards the response back to the rpi websocket client
-	tcpListener, err := net.Listen("tcp", "9001")
+	// start a tcp server that will forward ssh request to the websocket connection
+	tcpListener, err := net.Listen("tcp", "127.0.0.1:9001")
 	if err != nil {
+		log.Warningf("Failed to listen to TCP: %v", err)
 		return
 	}
 
-	// start a goroutine to handle the TCP server
-	go func() {
-		for {
-			tcpConn, err := tcpListener.Accept()
-			if err != nil {
-				return
-			}
+	// start a goroutine to handle the SSH request
+	for {
+		tcpConn, err := tcpListener.Accept()
+		if err != nil {
+			return
+		}
+		go func() {
+			ctx, cancel := context.WithCancel(r.Context())
+			defer tcpConn.Close()
+			// handle the TCP connection
+			// read the request from the TCP connection
+			// copy websocket conn bytes to tcp conn
 			go func() {
-				defer tcpConn.Close()
-				// handle the TCP connection
-				// read the request from the TCP connection
-				// copy websocket conn bytes to tcp conn
-				go func() {
-					for {
-						select {
-						case <-ctx.Done():
-							return
-						default:
-							for {
-								mt, r, err := wsConn.NextReader()
-								if websocket.IsCloseError(err,
-									websocket.CloseNormalClosure,   // Normal.
-									websocket.CloseAbnormalClosure, // OpenSSH killed proxy client.
-								) {
-									return
-								}
-								if err != nil {
-									log.Errorf("nextreader: %v", err)
-									return
-								}
-								if mt != websocket.BinaryMessage {
-									log.Errorf("received non-binary websocket message")
-									return
-								}
-								if _, err := io.Copy(tcpConn, r); err != nil {
-									log.Warningf("Reading from websocket: %v", err)
-									cancel()
-								}
-							}
-						}
-					}
-				}()
-				// copy tcp conn bytes to websocket conn
 				for {
 					select {
 					case <-ctx.Done():
 						return
 					default:
-						buf := make([]byte, 1024)
-						n, err := tcpConn.Read(buf)
-						if err != nil {
-							log.Warningf("Reading from file: %v", err)
-							cancel()
-							return
-						}
-						if err := wsConn.WriteMessage(websocket.BinaryMessage, buf[:n]); err != nil {
-							log.Warningf("Writing to websocket: %v", err)
-							cancel()
-							return
+						for {
+							mt, r, err := wsConn.NextReader()
+							if websocket.IsCloseError(err,
+								websocket.CloseNormalClosure,   // Normal.
+								websocket.CloseAbnormalClosure, // OpenSSH killed proxy client.
+							) {
+								return
+							}
+							if err != nil {
+								log.Errorf("nextreader: %v", err)
+								return
+							}
+							if mt != websocket.BinaryMessage {
+								log.Errorf("received non-binary websocket message")
+								return
+							}
+							if _, err := io.Copy(tcpConn, r); err != nil {
+								log.Warningf("Reading from websocket: %v", err)
+								cancel()
+							}
 						}
 					}
 				}
 			}()
-		}
-	}()
+			// copy tcp conn bytes to websocket conn
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					buf := make([]byte, 1024)
+					n, err := tcpConn.Read(buf)
+					if err != nil {
+						log.Warningf("Reading from file: %v", err)
+						cancel()
+						return
+					}
+					if err := wsConn.WriteMessage(websocket.BinaryMessage, buf[:n]); err != nil {
+						log.Warningf("Writing to websocket: %v", err)
+						cancel()
+						return
+					}
+				}
+			}
+		}()
+	}
 }
 
 func main() {
@@ -138,7 +135,7 @@ func main() {
 
 	log.Infof("huproxy %s", huproxy.Version)
 	m := mux.NewRouter()
-	m.HandleFunc(fmt.Sprintf("/%s/{host}/{port}", *url), handleProxy)
+	m.HandleFunc("/", handleProxy)
 	s := &http.Server{
 		Addr:           *listen,
 		Handler:        m,
